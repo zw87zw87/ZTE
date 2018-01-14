926c7e30cfa04691fead5ad6fef8b40b673b175f
ZTE <zw87zw87@163.com>
2018-01-14 22:05:57 +0800

    "book"

---
+++ src/main/java/quiz/BookAccount.java
@@ -0,0 +1,24 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class BookAccount {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String book = in.nextLine();
+        float account = getAccount(book);
+        System.out.println("图书(" + book +")的折扣系数为：" + account);
+    }
+    public static float getAccount(String book){
+        float account = 0.0f;
+        if (book.equals("新书")){
+            account = 1.2f;
+        }else if (book.equals("常规图书")){
+            account = 1.0f;
+        }else if (book.equals("滞销图书")){
+            account = 0.6f;
+        }
+        return account;
+    }
+}
